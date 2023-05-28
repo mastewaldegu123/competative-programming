f0635a8f6ab62675723b65dee5b7e1dc9896b88f
mastewal degu <126682436+mastewaldegu123@users.noreply.github.com>
2023-05-28 14:24:10 +0300

    Create find-target-indices-after-sorting-array.py

---
+++ find-target-indices-after-sorting-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        final=[]
+        for i in range(len(nums)):
+            if nums[i] == target:
+                final.append(i)
+        return final
+
+          
